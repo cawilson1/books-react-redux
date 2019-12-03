9ec98617550d08c3b5163c7a49aa7280f7877d15
ripology <56270337+ripology@users.noreply.github.com>
2019-12-03 15:45:24 -0500

    terrible job

--- books-react-redux/src/components/Details.jsx
+++ books-react-redux/src/components/Details.jsx
@@ -1,13 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from '@reach/router'
+import axios from 'axios';
 
-function Details() {
-    const [properties, setProperties] = useState("");
 
- return (
-     <div>
-         <p>hola guapos</p>
-     </div>
- )
+
+function Details( props ) {
+    const [properties, setProperties] = useState({})
+    const [hasRendered, setHasRendered] = useState(false)
+    async function requestBooks() {
+        const books = await axios.get("http://localhost:2500/books")
+        console.log("books", books);
+        setProperties(books || []);
+        console.log(properties)
+    }
+    if (!hasRendered){
+        setHasRendered(true)
+        requestBooks() 
+    }
+    
+   
+    if(Object.keys(props).length === 0){
+        return <div>Loading</div>
+    }
+    return (
+        <div>{console.log((properties.data))}</div>
+    )
+    
 }
 
 export default Details
